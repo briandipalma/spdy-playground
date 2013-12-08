767a2feeca99d94eb5ec3d60439b08616193573c
Brian Di Palma <offler@gmail.com>
2013-12-08 12:49:31 +0000

    Generate test page.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-*Setup
+Setup
+-----
 
 <code>npm install</code>.
 
 <code>bower install</code>.
+
+To Create a test web page with the resources that are to be loaded via SPDY run:
+
+<code>node create-test-page.js [numberOfJsFilesToLoad] [numberOfCSSFilesToLoad]</code>.
+
+This will generate an index.html page with the required number of <code><script></code>
+and <code><link></code> tags.
--- create-test-page.js
+++ create-test-page.js
@@ -4,15 +4,51 @@
 //and 10 link tags with src="css/cssfile0-100.css"
 "use strict";
 
+var fs = require("fs");
+var bower = require("bower");
+
 var numberOfJSFilesToLoad = parseInt(process.argv[2], 10);
 var numberOfCSSFilesToLoad = parseInt(process.argv[3], 10);
 
 if(Number.isNaN(numberOfJSFilesToLoad)) {
     numberOfJSFilesToLoad = 100;
+
     console.info("Number of JS files not provided as first argument using default of", numberOfJSFilesToLoad);
 }
 
 if(Number.isNaN(numberOfCSSFilesToLoad)) {
     numberOfCSSFilesToLoad = numberOfJSFilesToLoad / 10;
+
     console.info("Number of CSS files not provided as second argument using default of", numberOfCSSFilesToLoad);
 }
+
+var scriptBlock = "";
+
+for (var i = 0; i < numberOfJSFilesToLoad; i++) {
+    scriptBlock += '        <script src="js/javascript' + i + '.js"></script>\n';
+}
+
+var cssBlock = "";
+
+for (var i = 0; i < numberOfCSSFilesToLoad; i++) {
+    cssBlock += '       <link rel="stylesheet" href="css/css' + i + '.css">\n';
+}
+
+var htmlTemplate = "<!DOCTYPE html>\n" +
+                   "<html>\n" +
+                   "    <head>\n" +
+                   "        <title>Title</title>\n" +
+                   '        <meta charset="UTF-8">\n' +
+                   "{{ CSSBlock }}\n" +
+                   "{{ JSBlock }}\n" +
+                   "    </head>\n" +
+                   "    <body>\n" +
+                   "    </body>\n" +
+                   "</html>";
+
+var htmlTestPageMarkup = htmlTemplate.
+        replace("{{ CSSBlock }}", cssBlock).
+        replace("{{ JSBlock }}", scriptBlock);
+
+fs.writeFileSync("index.html", htmlTestPageMarkup);
+
