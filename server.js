ee1b4120c5dfc6b8e5333cadfd7e53ca806f25d5
Brian Di Palma <offler@gmail.com>
2013-12-17 22:03:46 +0000

    Adding a function to load up all the files requested by the index page.

--- server.js
+++ server.js
@@ -9,8 +9,29 @@ var options = {
         cert: fs.readFileSync(__dirname + "/keys/server.crt")
     };
 
-var indexhtml = fs.readFileSync('index.html'),
+var jsFiles = [],
+    cssFiles = [],
+    indexhtml = fs.readFileSync('index.html'),
     server = spdy.createServer(options, requestReceived);
+
+/**
+ * Load all files that will be requested on index load, to allow us to push the files to the browser.
+ * 
+ * @param {type} numberOfFiles
+ * @param {type} fileArray
+ * @param {type} prepend
+ * @param {type} append
+ * @returns {undefined}
+ */
+function loadAllResourceFiles(numberOfFiles, fileArray, prepend, append) {
+	for (var file = 0; file < numberOfFiles; file++) {
+		fileArray.push(fs.readFileSync(__dirname + prepend + file + append));
+	}
+}
+
+loadAllResourceFiles(100, jsFiles, "/js/javascript", ".js");
+
+console.info("Loaded all Js files");
 
 function requestReceived(request, response) {
     console.info(request.url, "Request");
@@ -18,13 +39,21 @@ function requestReceived(request, response) {
     if(request.url === "/") {
         var headers = {
             "Content-Type": "text/html"
-        }
+        };
         headers["Content-Length"] = indexhtml.length;
 
         response.writeHead(200, headers);
         response.end(indexhtml);
     }
 
+    
+    
+//    if (request.isSpdy){
+//    message = "YAY! SPDY Works!"
+//  }
+};
+
+function pushRequiredResource() {
 //    var headers = {
 //        "content-type": "application/javascript"
 //    };
@@ -34,14 +63,6 @@ function requestReceived(request, response) {
 //
 //      stream.end(backbone);
 //    });
-    
-    
-//    if (request.isSpdy){
-//    message = "YAY! SPDY Works!"
-//  }
-};
-
-function pushRequiredResource() {
     
 }
 
